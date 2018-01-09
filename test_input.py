680e69ed4627e1c889db168b447ca1a70a0a28d5
Javi <javi.lazcano@gmail.com>
2018-01-09 21:28:27 +0100

    Adivina un numero

---
+++ test_input.py
@@ -0,0 +1,11 @@
+
+numero_ganador = 7
+
+numero_usuario = int(input("Adivina el numero: "))
+
+if numero_usuario == numero_ganador:
+    print("HAS GANADO!!!")
+else:
+    print ("HAS PERDIDO!!!")
+
+
