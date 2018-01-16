a30f40a2fab808fe8c852cd2f8ce257cc80e36a7
Javi <javi.lazcano@gmail.com>
2018-01-16 18:57:25 +0100

    Adivina un numero

--- cuenta_letras.py
+++ cuenta_letras.py
@@ -2,11 +2,14 @@
 Contar vocales y consonantes de una frase
 """
 
+""" FORMA 1 - CON LISTAS """
+
 vocales = ["a", "e", "i", "o", "u"]
 total_vocales = 0
 total_consonantes = 0
 
 phrase = input("Dime una frase: ")
+
 for i in phrase:
     if i in vocales:
         total_vocales += 1
@@ -15,3 +18,21 @@ for i in phrase:
 
 print("TOTAL DE VOCALES: {}".format(str(total_vocales)))
 print("TOTAL DE CONSONANTES: {}".format(str(total_consonantes)))
+
+""" FORMA 2 - SIN LISTAS """
+
+vocales = "aeiou"
+consonantes = "bcdfghjklmnñpqrstvwxyz"
+total_vocales = 0
+total_consonantes = 0
+
+frase = input("Frase: ")
+
+for i in frase:
+    if i in vocales:
+        total_vocales += 1
+    if i in consonantes:
+        total_consonantes += 1
+
+print("Total vocales: {}".format(total_vocales))
+print("Total consonantes: {}".format(total_consonantes))
