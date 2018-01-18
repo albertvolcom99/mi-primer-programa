129b1c6219eab5731fe0c92bbbf9661b9033834f
Javi <javi.lazcano@gmail.com>
2018-01-18 17:49:59 +0100

    Multiplica un numero del 5 al 15

--- multiplicar_cinco.py
+++ multiplicar_cinco.py
@@ -2,7 +2,7 @@
 Multiplicar del 5 al 15
 """
 
-numero = int(input("Número: "))
+numero = int(input("Número a multiplicar: "))
 
 for i in range(5,16):
     print("{} X {} = {}".format(numero, i, numero*i))
