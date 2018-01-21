ad245aa96a657c80e98eabfdbccc142ae29a440d
Javi <javi.lazcano@gmail.com>
2018-01-21 22:06:19 +0100

    Multiplica los números de una lista

---
+++ multiplica_lista.py
@@ -0,0 +1,11 @@
+"""
+Dada una lista de números, devolver el resultado de la multiplicación de todos los números
+"""
+
+lista_numeros = [3, 5, 2, 8, 7]
+resultado = 1
+
+for i in lista_numeros:
+    resultado *= i
+
+print(resultado)
